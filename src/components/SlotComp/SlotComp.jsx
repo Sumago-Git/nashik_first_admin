a0bf98ed923f58d12b45bfa2fc9311e4afdb2875
shubham <shubham.kothavade09@gmail.com>
2024-11-13 00:48:06 +0530

    changes

--- src/components/SlotComp/SlotComp.jsx
+++ src/components/SlotComp/SlotComp.jsx
@@ -524,7 +524,7 @@ const SlotComp = ({ selectedDates, categoryName, showModal, handleCloseModal, ha
 
                             <Col md={10}>
                                 <Form.Group controlId="trainingType">
-                                    <Form.Label>Training Type</Form.Label>
+                                    <Form.Label>Category</Form.Label>
                                     <Form.Select
                                         name="category"
                                         value={formData.category} // Use "category" as it matches initialFormData
@@ -545,7 +545,7 @@ const SlotComp = ({ selectedDates, categoryName, showModal, handleCloseModal, ha
 
                             <Col md={10}>
                                 <Form.Group controlId="trainingType">
-                                    <Form.Label>Training Type</Form.Label>
+                                    <Form.Label>Trainer Name</Form.Label>
                                     <Form.Select
                                         name="category"
                                         value={formData.trainer} // Use "category" as it matches initialFormData
